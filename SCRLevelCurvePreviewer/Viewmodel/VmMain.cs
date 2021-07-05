ce5d4209cf5416764ef68a9e76c08557c1249c85
Justin113D <Justin113D@gmail.com>
2021-07-05 20:32:00 +0200

    Added level curve previewer

---
+++ SCRLevelCurvePreviewer/Viewmodel/VmMain.cs
@@ -0,0 +1,128 @@
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using SCRCommon.Viewmodels;
+using SCRLevelCurvePreviewer.Data;
+using System;
+using System.Collections.ObjectModel;
+
+namespace SCRLevelCurvePreviewer.Viewmodel
+{
+    public class VmMain : BaseViewModel
+    {
+        public PlotModel ExperienceModel { get; private set; }
+
+        private ulong _expMin = 200;
+        private ulong _expMax = 1000000000;
+        private double _expXShift = 0;
+        private double _expYShift = 1;
+
+        public double ExpMin
+        {
+            get => _expMin;
+            set
+            {
+                ulong val = (ulong)Math.Round(value);
+                if(val == _expMin)
+                    return;
+                _expMin = val;
+                UpdatePlot();
+            }
+        }
+
+        public double ExpMax
+        {
+            get => _expMax;
+            set
+            {
+                ulong val = (ulong)Math.Round(value);
+                if(val == _expMax)
+                    return;
+                _expMax = val;
+                UpdatePlot();
+            }
+        }
+
+        public double ExpXShift
+        {
+            get => _expXShift;
+            set
+            {
+                if(value == _expXShift)
+                    return;
+                _expXShift = value;
+                UpdatePlot();
+            }
+        }
+
+        public double ExpYShift
+        {
+            get => _expYShift;
+            set
+            {
+                if(value == _expYShift)
+                    return;
+                _expYShift = value;
+                UpdatePlot();
+            }
+        }
+
+        private ExpCalculator _expCalculator;
+
+        private LineSeries _series;
+
+        public VmMain()
+        {
+            _series = new LineSeries()
+            {
+                Title = "Experience per level"
+            };
+
+            UpdatePlot();
+            
+            ExperienceModel = new PlotModel()
+            {
+                Title = "Exp growth graph",
+                
+            };
+
+            ExperienceModel.Axes.Add(new LinearAxis()
+            {
+                Position = AxisPosition.Bottom,
+                Title = "Level",
+                IsZoomEnabled = false,
+                IsPanEnabled = false
+            });
+
+            ExperienceModel.Axes.Add(new LinearAxis()
+            {
+                Position = AxisPosition.Left,
+                Title = "Experience",
+                IsZoomEnabled = false,
+                IsPanEnabled = false
+            });
+
+            ExperienceModel.Series.Add(_series);
+
+        }
+
+        private double f(double x)
+        {
+            return _expCalculator.GetExperience((uint)x);
+        }
+
+        private void UpdatePlot()
+        {
+            _expCalculator = new ExpCalculator(_expMin, _expMax, _expXShift, _expYShift);
+
+            _series.Points.Clear();
+            for(uint x = 1; x <= 100; x++)
+                _series.Points.Add(new DataPoint(x, _expCalculator.GetExperience(x)));
+
+            if(ExperienceModel != null)
+            {
+                ExperienceModel.InvalidatePlot(true);
+            }
+        }
+    }
+}
---
+++ SCRLevelCurvePreviewer/XAML/UcValueSlider.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace SCRLevelCurvePreviewer.XAML
+{
+    /// <summary>
+    /// Interaction logic for UCValueSlider.xaml
+    /// </summary>
+    public partial class UcValueSlider : UserControl
+    {
+        #region Properties
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(
+                nameof(Label),
+                typeof(string),
+                typeof(UcValueSlider),
+                new FrameworkPropertyMetadata(
+                    ""
+                )
+            );
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        public static readonly DependencyProperty IsFloatProperty =
+            DependencyProperty.Register(
+                nameof(IsFloat),
+                typeof(bool),
+                typeof(UcValueSlider),
+                new FrameworkPropertyMetadata(
+                    false
+                )
+            );
+
+        public bool IsFloat
+        {
+            get => (bool)GetValue(IsFloatProperty);
+            set => SetValue(IsFloatProperty, value);
+        }
+
+        public static readonly DependencyProperty LabelWidthProperty =
+            DependencyProperty.Register(
+                nameof(LabelWidth),
+                typeof(double),
+                typeof(UcValueSlider),
+                new FrameworkPropertyMetadata(
+                    50d
+                )
+            );
+
+        public double LabelWidth
+        {
+            get => (double)GetValue(LabelWidthProperty);
+            set => SetValue(LabelWidthProperty, value);
+        }
+
+        public static readonly DependencyProperty TextFieldWidthProperty =
+            DependencyProperty.Register(
+                nameof(TextFieldWidth),
+                typeof(double),
+                typeof(UcValueSlider),
+                new FrameworkPropertyMetadata(
+                    100d
+                )
+            );
+
+        public double TextFieldWidth
+        {
+            get => (double)GetValue(TextFieldWidthProperty);
+            set => SetValue(TextFieldWidthProperty, value);
+        }
+
+        public static readonly DependencyProperty SliderValueProperty =
+            DependencyProperty.Register(
+                nameof(SliderValue),
+                typeof(double),
+                typeof(UcValueSlider),
+                new FrameworkPropertyMetadata(
+                    0d
+                )
+            );
+
+
+        public double SliderValue
+        {
+            get => (double)GetValue(SliderValueProperty);
+            set => SetValue(SliderValueProperty, value);
+        }
+
+        public static readonly DependencyProperty SliderMaxProperty =
+            DependencyProperty.Register(
+                nameof(SliderMax),
+                typeof(double),
+                typeof(UcValueSlider),
+                new FrameworkPropertyMetadata(
+                    0d
+                )
+            );
+
+
+        public double SliderMax
+        {
+            get => (double)GetValue(SliderMaxProperty);
+            set => SetValue(SliderMaxProperty, value);
+        }
+
+        #endregion
+
+        private static readonly Regex _regex = new Regex("[^0-9.-]+");
+
+        public UcValueSlider()
+        {
+            InitializeComponent();
+        }
+
+        private void Text_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = _regex.IsMatch(e.Text);
+        }
+
+    }
+}
