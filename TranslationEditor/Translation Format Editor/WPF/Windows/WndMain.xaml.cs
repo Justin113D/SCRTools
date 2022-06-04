a388dfb66a652f18d01e8b1514e678e18158963f
Justin113D <Justin113D@gmail.com>
2022-06-04 22:30:20 +0200

    Renamed folder again

---
+++ TranslationEditor/Translation Format Editor/WPF/Windows/WndMain.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SCR.Tools.WPF.Styling;
+
+namespace SCR.Tools.TranslationEditor.FormatEditor.WPF.Windows
+{
+    /// <summary>
+    /// Interaction logic for WndMain.xaml
+    /// </summary>
+    public partial class WndMain : Window
+    {
+        public WndMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
