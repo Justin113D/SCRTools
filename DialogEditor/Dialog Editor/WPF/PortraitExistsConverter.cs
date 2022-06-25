a5f76e9044761040814ea774968e4b83cb14a1fa
Justin113D <Justin113D@gmail.com>
2022-06-25 22:17:32 +0200

    Dialog editor - "Portrait exists" indicator added to expression/character dropdown

---
+++ DialogEditor/Dialog Editor/WPF/PortraitExistsConverter.cs
@@ -0,0 +1,40 @@
+﻿using SCR.Tools.DialogEditor.Viewmodeling;
+using SCR.Tools.WPF.Styling;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media;
+using Color = System.Drawing.Color;
+using Colors = SCR.Tools.WPF.Styling.Colors;
+
+namespace SCR.Tools.DialogEditor.WPF
+{
+    public class PortraitExistsConverter : IMultiValueConverter
+    {
+        private static readonly SolidColorBrush Green = new(Colors.Green);
+        private static readonly SolidColorBrush Transparent = new(System.Windows.Media.Colors.Transparent);
+        private static readonly SolidColorBrush Red = new(Colors.Red);
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            string? folder = (string?)values[0];
+            VmNodeOption<Color>? character = (VmNodeOption<Color>?)values[1];
+            VmNodeOption<Color>? expression = (VmNodeOption<Color>?)values[2];
+
+            if(folder == null || character == null || expression == null)
+            {
+                return Transparent;
+            }
+
+            return File.Exists($"{folder}/{character}_{expression}.png")
+                ? Green
+                : Red;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
